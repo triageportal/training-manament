99a0a0a760efb2ebc5303ff388d86cf2b6925d1d
bahti <bahtirek@gmail.com>
2021-05-18 23:28:35 -0400

    ser details in progress

--- src/app/dashboard/users/user-details/user-details.component.ts
+++ src/app/dashboard/users/user-details/user-details.component.ts
@@ -1,4 +1,9 @@
+import { ModalService } from './../../../shared/modal/modal.service';
+import { Modal } from './../../../interfaces/modal.interace';
+import { User } from './../../../interfaces/user.interface';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +12,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  user: User = {};
+
+  constructor(private route: ActivatedRoute, private modalService: ModalService,) { }
 
   ngOnInit() {
+    this.setUser();
+  }
+
+  setUser() {
+    this.user.name = this.route.snapshot.queryParams['name'];
+    this.user.email = this.route.snapshot.queryParams['email'];
+    this.user.id = this.route.snapshot.queryParams['id'];
+  }
+
+  openDeleteModal(){
+    this.modalService.modalSource.next(this.deleteModal);
+    this.modalService.modalResponseObservable.pipe(take(1)).subscribe(
+      result => {
+        console.log(result);
+        //this.deleteUser(); deltes user, opens success modal
+      }
+    )
+  }
+
+  openSuccessDeleteModal(){
+    this.modalService.modalSource.next(this.successDeleteModal);
+    this.modalService.modalResponseObservable.pipe(take(1)).subscribe(
+      result => {
+        console.log(result);
+        
+      }
+    )
+  }
+  openErrorModal(){
+    this.modalService.modalSource.next(this.errorModal);
+    this.modalService.modalResponseObservable.pipe(take(1)).subscribe(
+      result => {
+        console.log(result);
+        
+      }
+    )
+  }
+
+  
+  deleteModal: Modal = {
+    size: 'sm',
+    title: 'Delete User?',
+    titleType: 'danger',
+    buttons: [{name: 'Delete', type: 'primary'}, {name: 'Cancel', type: 'danger'}]
+  }
+
+
+  successDeleteModal: Modal = {
+    size: 'sm',
+    title: 'User deleted',
+    titleType: 'primary',
+    buttons: [{name: 'ok', type: 'primary'}]
+  }
+
+  errorModal: Modal = {
+    size: 'sm',
+    title: 'Oops!',
+    titleType: 'Primary',
+    description: ['Something went wrong.', 'Please try later!'],
+    descriptionType: 'danger',
+    buttons: [{name: 'ok', type: 'primary'}]
   }
 
 }
